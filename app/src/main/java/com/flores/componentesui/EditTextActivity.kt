dfa0c12eede24037c85c233adf17d778a60b8d78
AlexanderF <alexander.flores.r@tecsup.edu.pe>
2022-04-15 23:42:01 -0500

    Lab 03 - ComponentesUI

---
+++ app/src/main/java/com/flores/componentesui/EditTextActivity.kt
@@ -0,0 +1,11 @@
+package com.flores.componentesui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditTextActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_text)
+    }
+}
---
+++ app/src/main/java/com/flores/componentesui/ToogleButtonActivity.kt
@@ -0,0 +1,11 @@
+package com.flores.componentesui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ToogleButtonActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_toogle_button)
+    }
+}
